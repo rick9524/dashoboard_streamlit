842d64bc9fcfed9da400ea483a1807dc36f62c31
rick <rk22366003@ga.ttc.ac.jp>
2023-02-09 12:38:33 +0900

    '設定'

---
+++ report.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from scipy import stats
+from scipy import integrate
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Meiryo'
+import streamlit as st
+from PIL import Image
+
+df = pd.read_csv('ポケモン種族値.csv')
+st.plot(df[''])
+plt.show()
